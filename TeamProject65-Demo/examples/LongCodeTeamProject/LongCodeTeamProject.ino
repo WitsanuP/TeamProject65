0362c31b6f22c2e02b8b27ec73c7a26c5ef24e0e
Balor <122012816+WitsanuP@users.noreply.github.com>
2023-04-11 14:08:00 +0700

    Add files via upload

---
+++ TeamProject65-Demo/examples/LongCodeTeamProject/LongCodeTeamProject.ino
@@ -0,0 +1,13 @@
+//วิธีใช้ https://github.com/WitsanuP/TeamProject65/blob/main/HowToUse.txt
+#include <witsanu.h>
+
+void setup(){
+  gost();                       //setup
+}
+
+void loop() {
+
+  Blend285(1000);                //สร้าง 3 เหลี่ยม      <time>
+  CaptainMorgan("Tommy",1000);   //เขียนชื่อ            <ชื่อ><time>
+  JohnnyWalker(1357,1000);       //เขียนรหัสนศพร้อมรูป  <รหัสนศ><time>
+}
---
+++ TeamProject65-Demo/examples/ShortCodeTeamProject/ShortCodeTeamProject.ino
@@ -0,0 +1,10 @@
+//วิธีใช้ https://github.com/WitsanuP/TeamProject65/blob/main/HowToUse.txt
+#include <witsanu.h>
+
+void setup(){
+  gost();                  //setup
+}
+
+void loop() {
+  SoSad("Tommy",1357);     //เขียนชื่อ และรหัสนศ    <ชื่อ><รหัสนศ>
+}
